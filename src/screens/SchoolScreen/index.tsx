c40dbe72cb62e72c2a63b2970b2d0ea25aad80c6
alagh697 <37829292+alagh697@users.noreply.github.com>
2024-04-09 07:20:43 +0200

    map commit by Alaaeddine

--- src/screens/SchoolScreen/index.tsx
+++ src/screens/SchoolScreen/index.tsx
@@ -1,11 +1,24 @@
 import { View, Text, StyleSheet } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 import MapView from 'react-native-maps'
 
 export default function SchoolScreen() {
+  const [latitude, setLatitude] = useState(45.792100);
+  const [longitude, setLongitude] = useState(4.811380);
   return (
     <View style={styles.container}>
-      <MapView style={styles.map} />
+      <MapView style={styles.map} 
+      mapType="mutedStandard"
+      showsPointsOfInterest={false}
+      showsUserLocation={true}
+      followsUserLocation={false}
+      initialRegion={{
+      latitude: latitude,
+      longitude: longitude,
+      latitudeDelta: 0.010,
+      longitudeDelta: 0.010,
+      }}
+      />
     </View>
   )
 }
